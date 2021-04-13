8bfd18601447d9f9d67ff5c471e24f47ef296972
Angga Kurniawan <67410461+anggaxd@users.noreply.github.com>
2021-04-13 07:03:25 +0700

    Update enc.py

--- enc.py
+++ enc.py
@@ -16,7 +16,7 @@ logo = """     \033[0;91m██████  ██    ██ ████
  ➣ Instagram : @gaaarzxd
  ➣ Facebook  : https://fb.me/gaaaarzxd\n"""
 
-def menupy2():
+def menu():
 	os.system("clear")
 	print logo
 	print " [!] Masukan Nama File Nya Ajah Tanpa .py" 
@@ -31,29 +31,5 @@ def menupy2():
 	print " [*] Oke Sudah Di Encrypt"
 	print " [+] Nama File : "+str(sv_file)+".py"
 
-def menupy3():
-	os.system("clear")
-	print logo
-	print " [!] Masukan Nama File Nya Ajah Tanpa .py" 
-	print " [*] Contoh : pyenc.py (masukan filenya : pyenc)" 
-	name_file = raw_input(" [+] Masukan File : ")
-	saved_file = raw_input(" [+] Simpan File : ")
-	py_dato = ("__pycache__/"+str(name_file)+".cpython-38.opt-2.pyc")
-	print " [#] Bentar Lagi Encrypt SC Nya...."
-	time.sleep(1)
-	os.system('python -OO -m py_compile ' + name_file+'.py')
-	os.system("mv "+str(py_dato)+" "+str(saved_file)+".py")
-	os.system('rm -rf __pycache__')
-	print " [*] Oke Sudah Di Encrypt"
-	print " [+] Nama File : "+str(saved_file)+".py"
-	
 if __name__ == '__main__':
-	if len(sys.argv) == 2:
-		if sys.argv[1] == 'enc_py2':
-			menupy2()
-		if sys.argv[1] == 'enc_py3':
-			menupy3()
-		else:
-			print " [!] Cara Menggunakan Nya :"
-			print " [+] Encrypt Python2 Ketik : python2 enc.py enc_py2"
-			exit (" [+] Encrypt Python3 Ketik : python2 enc.py enc_py3") 
+	menu()
